3684b5329f3d1cdb5ce8fecd6f9fd0cb66d21053
KUMATV <KUMATV@LAPTOP-C82971OS>
2022-03-19 21:27:11 -0400

    Updated Files ICE 9

---
+++ server.ts
@@ -0,0 +1,36 @@
+import http from 'http';
+import fs from 'fs';
+import mime from 'mime-types';
+let lookup = mime.lookup;
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(function(req, res)
+{
+    let path = req.url as string;
+    if (path == "/" || path == "/home")
+    {
+        path = "/index.html";
+    }
+
+    let mime_type = lookup(path.substring(1)) as string;
+
+    console.log(path);
+
+    fs.readFile(__dirname  + path, function(err, data)
+    {
+        if (err)
+        {
+            res.writeHead(404);
+            res.end("ERROR: 404 - File Not Found! " + err.message);
+            return;
+        }
+        res.setHeader("X-Content-TypeOptions", "nosniff"); 
+        res.writeHead(200, {'Content-Type': mime_type});
+        res.end(data);
+    });
+});
+
+server.listen(port, function() {
+  console.log(`Server running at Port"${port}/`);
+});
