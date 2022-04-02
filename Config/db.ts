99941871d9b1a1c1524ac6fe77fe8681051de15c
KUMATV <KUMATV@LAPTOP-C82971OS>
2022-04-02 12:38:03 -0400

    Updated For ICE 11

---
+++ Config/db.ts
@@ -0,0 +1,4 @@
+export const LocalURI = "mongodb://localhost/contacts";
+export const RemoteURI = "mongodb+srv://moralesa:2oFnce5baGIyuyFp@cluster0.tg7if.mongodb.net/contacts?retryWrites=true&w=majority";
+export const HostName = "MongoDB Altas";
+export const SessionSecret = "WEBD6201SessionnSecret";
